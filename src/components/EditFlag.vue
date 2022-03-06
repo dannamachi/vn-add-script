6fce6f62f7d5343380d87bed7638e9647e6486cd
dannamachi <serenita15@gmail.com>
2022-03-06 17:02:08 +1100

    remove ids

--- src/components/EditFlag.vue
+++ src/components/EditFlag.vue
@@ -2,7 +2,7 @@
 <div class='container'>
     <div v-if='!context.editing' class='row'>
         <div class='col-sm-4 d-flex align-items-start'>
-            <div class="nav nav-pills flex-column me-6" id="myTab" role="tablist" aria-orientation="vertical">
+            <div class="nav nav-pills flex-column me-6" role="tablist" aria-orientation="vertical">
                 <button class="nav-link active" id="flag-flag-tab" data-bs-toggle="pill" data-bs-target="#flag-flag" type="button" role="tab" aria-controls="flag-flag" aria-selected="true">{{ context.setterFlag ? 'flag' : 'has flag'}}</button>
 
                 <button class="nav-link" id="flag-score-tab" data-bs-toggle="pill" data-bs-target="#flag-score" type="button" role="tab" aria-controls="flag-score" aria-selected="false">{{ context.setterFlag ? 'score' : 'equal score'}}</button>
@@ -20,7 +20,7 @@
                 <button v-if="context.setterFlag" class="nav-link" id="flag-dec-tab" data-bs-toggle="pill" data-bs-target="#flag-dec" type="button" role="tab" aria-controls="flag-dec" aria-selected="false">decrease score</button>
             </div>
         </div>
-        <div class="tab-content col-sm-8" id="myTabContent">
+        <div class="tab-content col-sm-8">
             <p class='mb-5'>Enter to add new {{ context.setterFlag ? 'setter' : 'condition' }} flag:</p>
             <div class="tab-pane show active" id="flag-flag" role="tabpanel" aria-labelledby="flag-flag-tab">
                 {{ context.setterFlag ? "give " : 'need '}}<input type="text" placeholder='flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('flag')"/>
