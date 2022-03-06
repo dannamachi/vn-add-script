ead9bafe4c0612ce8fc4267a808461674bfbab18
dannamachi <serenita15@gmail.com>
2022-03-06 15:38:20 +1100

    add inc/decrement flags

--- src/components/EditFlag.vue
+++ src/components/EditFlag.vue
@@ -14,10 +14,14 @@
                 <button class="nav-link" id="flag-value-tab" data-bs-toggle="pill" data-bs-target="#flag-value" type="button" role="tab" aria-controls="flag-value" aria-selected="false">{{ context.setterFlag ? 'value' : 'equal value'}}</button>
 
                 <button v-if="!context.setterFlag" class="nav-link" id="flag-diff-tab" data-bs-toggle="pill" data-bs-target="#flag-diff" type="button" role="tab" aria-controls="flag-diff" aria-selected="false">not value</button>
+
+                <button class="nav-link" id="flag-inc-tab" data-bs-toggle="pill" data-bs-target="#flag-inc" type="button" role="tab" aria-controls="flag-inc" aria-selected="false">increase score</button>
+
+                <button class="nav-link" id="flag-dec-tab" data-bs-toggle="pill" data-bs-target="#flag-dec" type="button" role="tab" aria-controls="flag-dec" aria-selected="false">decrease score</button>
             </div>
         </div>
         <div class="tab-content col-sm-8" id="myTabContent">
-            <p class='mb-5'>Enter to add new flag:</p>
+            <p class='mb-5'>Enter to add new {{ context.setterFlag ? 'setter' : 'condition' }}flag:</p>
             <div class="tab-pane show active" id="flag-flag" role="tabpanel" aria-labelledby="flag-flag-tab">
                 {{ context.setterFlag ? "give " : 'need '}}<input type="text" placeholder='flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('flag')"/>
             </div>
@@ -45,6 +49,16 @@
                 <p><input type="text" placeholder='flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('diff')"/></p>
                 <p>does not have the value</p>
                 <p><input type="text" placeholder='flag value' v-model="flagValue" maxlength="40" @keypress.enter.prevent="addExp('diff')"/></p>
+            </div>
+            <div class="tab-pane" id="flag-inc" role="tabpanel" aria-labelledby="flag-inc-tab">
+                <p><input type="text" placeholder='flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('inc')"/></p>
+                <p>increases by</p>
+                <p><input type="number" placeholder='increase value' v-model="flagScore" @keypress.enter.prevent="addExp('inc')"/></p>
+            </div>
+            <div class="tab-pane" id="flag-dec" role="tabpanel" aria-labelledby="flag-dec-tab">
+                <p><input type="text" placeholder='flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('dec')"/></p>
+                <p>decreases by</p>
+                <p><input type="number" placeholder='decrease value' v-model="flagScore" @keypress.enter.prevent="addExp('dec')"/></p>
             </div>
         </div>
     </div>
@@ -78,6 +92,16 @@
             <p><input type="text" placeholder='new flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('diff')"/>
             {{ " =/= " }}<input type="text" placeholder='new flag value' v-model="flagValue" maxlength="40" @keypress.enter.prevent="addExp('diff')"/></p>
         </div>
+        <div v-else-if='context.type == "inc"'>
+            <p>Enter to change from {{ this.context.oldName + " + " }}{{ this.context.oldScore }}</p>
+            <p><input type="text" placeholder='new flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('inc')"/>
+            {{ " < "}}<input type="number" placeholder='new increase value' v-model="flagScore" @keypress.enter.prevent="addExp('inc')"/></p>
+        </div>
+        <div v-else-if='context.type == "dec"'>
+            <p>Enter to change from {{ this.context.oldName + " - " }}{{ this.context.oldScore }}</p>
+            <p><input type="text" placeholder='new flag name' v-model="flagName" maxlength="40" @keypress.enter.prevent="addExp('dec')"/>
+            {{ " < "}}<input type="number" placeholder='new decrease value' v-model="flagScore" @keypress.enter.prevent="addExp('dec')"/></p>
+        </div>
         <div v-else>
             something went wrong ^^
         </div>
@@ -107,7 +131,7 @@ export default {
             if (flagType == 'flag' && stuff.name != "") {
                 this.$emit("addExp", stuff)
                 this.flagName = ""
-            } else if ((flagType == 'score' || flagType == 'more' || flagType == 'less') && stuff.name != "" && !isNaN(this.flagScore)) {
+            } else if ((flagType == 'score' || flagType == 'more' || flagType == 'less' || flagType == 'inc' || flagType == 'dec') && stuff.name != "" && !isNaN(this.flagScore)) {
                 stuff.score = this.flagScore
                 this.$emit("addExp", stuff)
                 this.flagName = ""
